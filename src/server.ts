ab0a07b02911282a9eecec40abc98540123f79dd
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-06 21:15:53 -0300

    feat: connection server :sparkles:

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
-import express from './app';
 import connection from './config/index';
 
-connection.then((conn) => {
-  express.app.listen(3000, () => {
-    console.log('Server running in port 3000');
-  });
+connection.then(() => {
+  require('./app').default.app.listen(3000, () => console.log('Conectou'));
 });
