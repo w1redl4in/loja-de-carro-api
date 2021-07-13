5535edacdc4f315a8e71fab2b8a4f555f4b437a7
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-13 15:27:55 -0300

    feat: :sparkles: connection catch

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,18 @@ import logger from '@middlewares/logger';
 import { dbConnection, server } from './config';
 import connection from './config/db';
 
-connection.then(() => {
-  logger.info(`Database connected: ${dbConnection.database}`);
-  logger.info(`Database syncronize: ${dbConnection.synchronize}`);
-  require('./app').default.app.listen(
-    server.port,
-    logger.info(
-      `Application is listening on port: ${server.port}/carshop/resources - server mode: ${server.env}`
-    )
-  );
-});
+connection
+  .then(() => {
+    logger.info(`Database connected: ${dbConnection.database}`);
+    logger.info(`Database syncronize: ${dbConnection.synchronize}`);
+    require('./app').default.app.listen(
+      server.port,
+      logger.info(
+        `Application is listening on port: ${server.port}/carshop/resources - server mode: ${server.env}`
+      )
+    );
+  })
+  .catch((err) => {
+    logger.error('Environment variables', { dbConnection, server });
+    logger.error(err);
+  });
